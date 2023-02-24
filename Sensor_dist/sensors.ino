f7911b3e269e0f94a1b4c7dabc885bb970533dca
jonathanw <jonwhewaycode@outlook.com>
2023-02-24 09:34:00 +0000

    removed redundant water sensor code

--- Sensor_dist/sensors.ino
+++ Sensor_dist/sensors.ino
@@ -1,4 +1,7 @@
-String path = PUBLISH_PATH + LOCATION + "/";
+void getTempHumData() {
+  temp = sht31.readTemperature() - 1;                   // Get current Temperature
+  hum = sht31.readHumidity();                           // Get current Humidity
+}
 
 bool sensors() { 
   static unsigned long wait_timer = 0;
@@ -192,6 +195,8 @@ bool sensors() {
   return false;
 }
 
+String path = PUBLISH_PATH + LOCATION + "/";
+
 void mqttPublishTempHumData(int num, String bedName) {
   if (isnan(temp)) {
     mqtt_client.publish(path + String("Sensor_") + String(num) + String("/") + String("bed_") + String(bedName) + String("/") + String("Temp ") , String("Sensor Disconnected"));
@@ -202,116 +207,6 @@ void mqttPublishTempHumData(int num, String bedName) {
     mqtt_client.publish(path + String("Sensor_") + String(num) + String("/") + String("bed_") + String(bedName) + String("/") + String("Hum ") , String(hum));
   }
 }
-
-void getTempHumData() {
-  temp = sht31.readTemperature() - 1;                   // Get current Temperature
-  hum = sht31.readHumidity();                           // Get current Humidity
-}
-
-//bool waterTempSensors() {
-//  static int water_sensor_number = 0;
-//  static unsigned long wait_timer = 0;
-//
-//  switch (water_sensor_number) {
-//    case 0:
-//      water_a.requestTemperatures();
-//      water_temp = water_a.getTempCByIndex(0);
-//      mqttPublishWaterTempData("A", water_temp_bed_name0);
-//      Serial.print(water_temp_bed_name0);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 1;
-//      break;
-//    case 1:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_b.requestTemperatures();
-//      water_temp = water_b.getTempCByIndex(0);
-//      mqttPublishWaterTempData("B", water_temp_bed_name1);
-//      Serial.print(water_temp_bed_name1);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 2;
-//      break;
-//    case 2:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_c.requestTemperatures();
-//      water_temp = water_c.getTempCByIndex(0);
-//      mqttPublishWaterTempData("C", water_temp_bed_name2);
-//      Serial.print(water_temp_bed_name2);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 3;
-//      break;
-//    case 3:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_d.requestTemperatures();
-//      water_temp = water_d.getTempCByIndex(0);
-//      mqttPublishWaterTempData("D", water_temp_bed_name3);
-//      Serial.print(water_temp_bed_name3);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 4;
-//      break;
-//    case 4:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_e.requestTemperatures();
-//      water_temp = water_e.getTempCByIndex(0);
-//      mqttPublishWaterTempData("E", water_temp_bed_name4);
-//      Serial.print(water_temp_bed_name4);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 5;
-//      break;
-//    case 5:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_f.requestTemperatures();
-//      water_temp = water_f.getTempCByIndex(0);
-//      mqttPublishWaterTempData("F", water_temp_bed_name5);
-//      Serial.print(water_temp_bed_name5);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 6;
-//      break;
-//    case 6:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_g.requestTemperatures();
-//      water_temp = water_g.getTempCByIndex(0);
-//      mqttPublishWaterTempData("G", water_temp_bed_name6);
-//      Serial.print(water_temp_bed_name6);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 7;
-//      break;
-//    case 7:
-//      if (millis() - wait_timer < interval_time ) {
-//        break;
-//      }
-//      water_h.requestTemperatures();
-//      water_temp = water_h.getTempCByIndex(0);
-//      mqttPublishWaterTempData("H", water_temp_bed_name7);
-//      Serial.print(water_temp_bed_name7);
-//      Serial.println(water_temp);
-//      wait_timer = millis();
-//      water_sensor_number = 0;
-//      return true;
-//      break;
-//  }
-//  return false;
-//}
-
-
 
 void mqttPublishWaterTempData(String letter, String water_temp_bed_name) {
   if (water_temp == -127)mqtt_client.publish(path + String("WaterSensor_") + String(letter) + String("/") + String("bed_") + String(water_temp_bed_name), String("Sensor Disconnected"));
